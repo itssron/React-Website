34fdda3bdb5462080d1b5258fcaf182d2bb29dbc
itssron <chorony605@gmail.com>
2024-10-17 20:07:37 -0400

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,83 @@
+import project1 from "../assets/projects/project-1.jpg";
+import project2 from "../assets/projects/project-2.jpg";
+import project3 from "../assets/projects/project-3.jpg";
+import project4 from "../assets/projects/project-4.jpg";
+
+export const HERO_CONTENT = `I am a passionate Software Engineer with a passion for Front End Development. With 2 years of hands-on experience from my classes and self-teaching, I have honed my skills in front-end technologies like React and Next.js, as well as Python, MySQL, Machine Learning, Javascript and R.`;
+
+export const ABOUT_TEXT = `I am a results-driven Computer Science student at the Commonwealth University of Pennsylvania, with a solid foundation in Python, Java, and SQL, and hands-on experience in web development and data management. With a passion for innovation and problem-solving, I have developed dynamic web solutions and worked in IT support, troubleshooting diverse hardware and software issues. Currently serving as the Secretary of the Computer Science Club, I’ve demonstrated leadership and teamwork skills by organizing club initiatives. I am actively expanding my skill set with React and Node.js, while pursuing opportunities in machine learning and data visualization. Eager to contribute to research and development in a collaborative environment, I am on track to complete my bachelor's degree in Spring 2025 and am actively seeking full-time opportunities where I can apply my technical expertise and drive for innovation.`;
+
+export const EXPERIENCES = [
+  {
+    year: " August 2024 - December 2024",
+    role: "Student IT Technician Intern",
+    company: "Lock Haven University IT Department",
+    description: `Communicated to support requests from end-users and patiently walked individuals
+through troubleshooting tasks. Managed system-wide operating system like Windows, Linux, Macintosh, and software
+deployments as well as related software upgrade problems. Diagnosed and troubleshot hardware, software, and network issues.
+Diagnosed and troubleshot hardware, software, and network issues. Assisted customer inquiries and provided technical assistance over the phone and in
+person. Collaborate with IT team members to streamline support processes and enhance
+overall efficiency, contributing to the department's success in delivering high-quality
+services to the university community.`,
+    technologies: ["Microsoft Intune","Virtual Machines", "Scripting", "Ticketing System"],
+  },
+  {
+    year: "December 2022 - Present",
+    role: "Student IT Technician",
+    company: "Lock Haven University IT Department",
+    description: `Communicated to support requests from end-users and patiently walked individuals
+through troubleshooting tasks. Managed system-wide operating system like Windows, Linux, Macintosh, and software
+deployments as well as related software upgrade problems. Diagnosed and troubleshot hardware, software, and network issues.
+Diagnosed and troubleshot hardware, software, and network issues. Assisted customer inquiries and provided technical assistance over the phone and in
+person. Collaborate with IT team members to streamline support processes and enhance
+overall efficiency, contributing to the department's success in delivering high-quality
+services to the university community.`,
+    technologies: ["Microsoft Intune","Virtual Machines", "Scripting", "Ticketing System"],
+  },
+  {
+    year: "October 2021 - December 2022",
+    role: "Dining Crew Member",
+    company: "Lock Haven University Dining, Aramark",
+    description: `Demonstrated ability to effectively coordinate and collaborate with team members in a
+fast-paced environment. Coordinated tasks, delegated responsibilities, and fostered a collaborative work
+environment to achieve common goals.`,
+technologies: [],
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "E-Commerce Website",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, shopping cart, and user authentication.",
+    technologies: ["HTML", "CSS", "React", "Node.js", "MongoDB"],
+  },
+  {
+    title: "Task Management App",
+    image: project2,
+    description:
+      "An application for managing tasks and projects, with features such as task creation, assignment, and progress tracking.",
+    technologies: ["HTML", "CSS", "Angular", "Firebase"],
+  },
+  {
+    title: "Portfolio Website",
+    image: project3,
+    description:
+      "A personal portfolio website showcasing projects, skills, and contact information.",
+    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+  },
+  {
+    title: "Blogging Platform",
+    image: project4,
+    description:
+      "A platform for creating and publishing blog posts, with features like rich text editing, commenting, and user profiles.",
+    technologies: ["HTML", "CSS", "Vue.js", "Express", "mySQL"],
+  },
+];
+
+export const CONTACT = {
+  address: "Lock Haven, PA, 17745 ",
+  phoneNo: "",
+  email: "chowdhurynoble14@gmail.com",
+};
